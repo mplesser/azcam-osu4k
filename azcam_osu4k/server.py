400f52d713e0ea29abd42d104cbc9cf3222fb61e
Michael Lesser <mlesser@arizona.edu>
2023-01-24 08:35:10 -0700

    update

--- azcam_osu4k/server.py
+++ azcam_osu4k/server.py
@@ -4,8 +4,8 @@ import sys
 import azcam
 import azcam.server
 import azcam.shortcuts
-from azcam.tools.cmdserver import CommandServer
-from azcam.tools.system import System
+from azcam.cmdserver import CommandServer
+from azcam.system import System
 from azcam.tools.instrument import Instrument
 from azcam.tools.telescope import Telescope
 from azcam.tools.archon.controller_archon import ControllerArchon
@@ -125,8 +125,8 @@ telescope.enabled = 0
 # ****************************************************************
 # read par file
 # ****************************************************************
-azcam.db.tools["parameters"].read_parfile(parfile)
-azcam.db.tools["parameters"].update_pars(0, "azcamserver")
+azcam.db.parameters.read_parfile(parfile)
+azcam.db.parameters.update_pars(0, "azcamserver")
 
 # ****************************************************************
 # define and start command server
